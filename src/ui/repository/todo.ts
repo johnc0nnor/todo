041d15fc1e1ab6dd389ffb4286be1ee4cecebd5b
Fabiano Mauro <fabianomauro@hotmail.com>
2024-04-23 21:24:57 -0400

    finalmente terminei aula 4.3

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -1,10 +1,8 @@
-import { allowedNodeEnvironmentFlags, cwd } from "process";
-
-interface todoRepositoryGetParams {
+interface TodoRepositoryGetParams {
     page: number;
     limit: number;
 }
-interface todoRepositoryGetOutput {
+interface TodoRepositoryGetOutput {
     todos: Todo[];
     total: number;
     pages: number;
@@ -12,33 +10,29 @@ interface todoRepositoryGetOutput {
 function get({
     page,
     limit,
-}: todoRepositoryGetParams): Promise<todoRepositoryGetOutput> {
-    return fetch("/api/todos").then(async (respostaDoServidor) => {
-        const todosString = await respostaDoServidor.text();
-        // Como garantir a tipagem de tipos desconhecidos?
-        const todosFromServer = parseTodosFromServer(
-            JSON.parse(todosString)
-        ).todos;
+}: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
+    return fetch(`/api/todos?page=${page}&limit=${limit}`).then(
+        async (respostaDoServidor) => {
+            const todosString = await respostaDoServidor.text();
+            // Como garantir a tipagem de tipos desconhecidos?
+            const responseParsed = parseTodosFromServer(
+                JSON.parse(todosString)
+            );
 
-        const ALL_TODOS = todosFromServer;
-        const startIndex = (page - 1) * limit;
-        const endIndex = page * limit;
-        const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex);
-        const totalPages = Math.ceil(ALL_TODOS.length / limit);
-
-        return {
-            todos: paginatedTodos,
-            total: ALL_TODOS.length,
-            pages: totalPages,
-        };
-    });
+            return {
+                total: responseParsed.total,
+                todos: responseParsed.todos,
+                pages: responseParsed.pages,
+            };
+        }
+    );
 }
 
 export const todoRepository = {
     get,
 };
 
-//Model/Schema
+// Model/Schema
 interface Todo {
     id: string;
     content: string;
@@ -46,14 +40,22 @@ interface Todo {
     done: boolean;
 }
 
-function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+function parseTodosFromServer(responseBody: unknown): {
+    total: number;
+    pages: number;
+    todos: Array<Todo>;
+} {
     if (
         responseBody !== null &&
         typeof responseBody === "object" &&
         "todos" in responseBody &&
+        "total" in responseBody &&
+        "pages" in responseBody &&
         Array.isArray(responseBody.todos)
     ) {
         return {
+            total: Number(responseBody.total),
+            pages: Number(responseBody.pages),
             todos: responseBody.todos.map((todo: unknown) => {
                 if (todo === null && typeof todo !== "object") {
                     throw new Error("Invalid todo from API");
@@ -65,6 +67,7 @@ function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
                     date: string;
                     done: string;
                 };
+
                 return {
                     id,
                     content,
@@ -74,7 +77,10 @@ function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
             }),
         };
     }
+
     return {
+        pages: 1,
+        total: 0,
         todos: [],
     };
 }
